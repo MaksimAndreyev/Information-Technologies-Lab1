7a13eeb5172e89c669495f22ddaaadb8fd1709b6
maximandreev <maxands@yandex.ru>
2024-04-21 12:34:00 +0300

    Int code deleted from GetRobotTarget

--- Tester/testGameSystem/GameSysTester.c
+++ Tester/testGameSystem/GameSysTester.c
@@ -1,14 +1,27 @@
 #include "GameSysTester.h"
 #include "GameSysTesterModules.h"
+#include <stdio.h>
+
+
+enum { TEST_SUCCESS, TEST_FAILURE };
 
 
 int testGameSystem()
 {
 	int code1 = testShoot();
 	int code2 = testPlaceShip();
+	FILE* testResults;
+	testResults = fopen("TestGameSystem.log", "w");
 	if (code1 == -1)
-		return -1;
+		fprintf(testResults, "Shoot failure\n");
 	if (code2 == -1)
-		return -2;
-	return 0;
+		fprintf(testResults, "PlaceShip failure\n");
+	if (code1 == -1 || code2 == -2)
+	{
+		fclose(testResults);
+		return TEST_FAILURE;
+	}
+	fprintf(testResults, "GameSystem successful\n");
+	fclose(testResults);
+	return TEST_SUCCESS;
 }
--- Tester/testRobotIO/RobotTester.c
+++ Tester/testRobotIO/RobotTester.c
@@ -1,14 +1,27 @@
 #include "RobotIOTesterModules.h"
 #include "RobotTester.h"
+#include <stdio.h>
+
+
+enum { TEST_SUCCESS, TEST_FAILURE };
 
 
 int testRobotIO()
 {
 	int code1 = testGetRobotShipKoords();
 	int code2 = testGetRobotTarget();
+	FILE* testResults;
+	testResults = fopen("TestRobotIO.log", "w");
 	if (code1 == -1)
-		return -1;
+		fprintf(testResults, "GetRobotShipKoords failure\n");
 	if (code2 == -1)
-		return -2;
-	return 0;
+		fprintf(testResults, "GetRobotTarget failure\n");
+	if (code1 == -1 || code2 == -2)
+	{
+		fclose(testResults);
+		return TEST_FAILURE;
+	}
+	fprintf(testResults, "RobotIO successful\n");
+	fclose(testResults);
+	return TEST_SUCCESS;
 }
